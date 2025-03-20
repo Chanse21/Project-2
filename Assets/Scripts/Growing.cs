d6291989f42de12968476a629a832266da4a054d
Chanse21 <47192175+Chanse21@users.noreply.github.com>
2025-03-20 19:14:37 -0400

    Added growing script to player

---
+++ Assets/Scripts/Growing.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Growing : MonoBehaviour
+{
+    public float growFactor = 1.5f; // How much to increase the scale
+    public float shrinkFactor = 0.5f; // How much to decrease the scale
+    public Vector3 minimumscale;
+    public Vector3 desiredscale;
+
+    
+    void Start()
+    {
+        Vector3 ms = minimumscale;
+        Vector3 ds = desiredscale;
+    }
+
+    void Update()
+    {
+        Vector3 minimumscale = transform.localScale;
+        Vector3 desiredscale = transform.localScale;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("GrowObject")) // Check if the colliding object has the "GrowObject" tag
+        {
+            transform.localScale *= growFactor; // Increase the scale
+        }
+        else if (collision.gameObject.CompareTag("ShrinkObject")) // Check if the colliding object has the "ShrinkObject" tag
+        {
+            transform.localScale *= shrinkFactor; // Decrease the scale
+        }
+    }
+
+   
+}
