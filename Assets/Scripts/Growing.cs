d4c97e1db9df0a0fb444cea7fa9cfedda8dae7d9
Chanse21 <47192175+Chanse21@users.noreply.github.com>
2025-03-25 19:03:05 -0400

    Update fixed growing script

--- Assets/Scripts/Growing.cs
+++ Assets/Scripts/Growing.cs
@@ -4,34 +4,31 @@ public class Growing : MonoBehaviour
 {
     public float growFactor = 1.5f; // How much to increase the scale
     public float shrinkFactor = 0.5f; // How much to decrease the scale
-    private Vector3 minimumscale, desiredscale;
-    private int ms, ds;
+    private Vector3 minimumscale, maximumscale;
 
     
     void Start()
     {
-        Vector3 ms = minimumscale;
-        Vector3 ds = desiredscale;
-        ms = new Vector3 (1f, 1f, 1f);
-        ds = new Vector3 (0.5f, 0.5f, 0.5f);
-    }
-
-    void Update()
-    {
-        Vector3 minimumscale = transform.localScale;
-        Vector3 desiredscale = transform.localScale;
-        
+       
+        minimumscale = new Vector3 (.1f, .1f, .1f);
+        maximumscale = new Vector3 (2f, 2f, 2f);
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("GrowObject")) // Check if the colliding object has the "GrowObject" tag
         {
-            transform.localScale *= growFactor; // Increase the scale
+            if ((transform.localScale * growFactor).magnitude < maximumscale.magnitude)
+            {
+                transform.localScale *= growFactor; // Increase the scale
+            }
         }
         else if (collision.gameObject.CompareTag("ShrinkObject")) // Check if the colliding object has the "ShrinkObject" tag
         {
-            transform.localScale *= shrinkFactor; // Decrease the scale
+            if ((transform.localScale * shrinkFactor).magnitude > minimumscale.magnitude)
+            {
+                transform.localScale *= shrinkFactor; // Increase the scale
+            }
         }
 
     }
